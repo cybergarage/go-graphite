a4ce1abebb3064edc08aa86496abfc3ac4db669d
Satoshi Konno <skonno@cybergarage.org>
2017-10-11 21:02:31 +0900

    * Updated Copyright

--- net/graphite/datapoint_test.go
+++ net/graphite/datapoint_test.go
@@ -1,4 +1,4 @@
-// Copyright 2017 The go-graphite Authors. All rights reserved.
+// Copyright (C) 2017 The go-graphite Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
