75d8e4a7a717b504591ad3fadd49f46e41d897f0
Satoshi Konno <skonno@cybergarage.org>
2024-08-21 12:41:44 +0900

    Add examle metrics store

---
+++ examples/go-graphited/server/metric/metrics_test.go
@@ -0,0 +1,14 @@
+// Copyright (C) 2017 Satoshi Konno. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package metric provides query interfaces for metric store.
+package metric
+
+import (
+	"testing"
+)
+
+func TestNewMetrics(t *testing.T) {
+	NewMetrics()
+}
